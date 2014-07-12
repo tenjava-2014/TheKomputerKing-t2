2db7e6be3049aad04758be2029f94a23a4d120bb
Kailan (TheKomputerKing) Blanks <kailan@komputerking.net>
2014-07-12 11:33:51 +0100

    Change theme.

--- src/main/java/com/tenjava/entries/TheKomputerKing/t2/Generators.java
+++ src/main/java/com/tenjava/entries/TheKomputerKing/t2/Generators.java
@@ -4,14 +4,14 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Powerups plugin - Allows survival users to find powerups
- * that can make the game experience longer.
+ * Generators plugin - adds generators to the game that can create fuel to power
+ * furnaces and other things that I haven't thought of yet.
  * @author kailanblanks
  */
-public class Powerups extends JavaPlugin {
+public class Generators extends JavaPlugin {
     
     private static Logger logger;
-    private static Powerups instance;
+    private static Generators instance;
     
     @Override
     public void onEnable() {
@@ -19,7 +19,7 @@ public class Powerups extends JavaPlugin {
         logger = this.getLogger();
     }
     
-    public static Powerups get() {
+    public static Generators get() {
         return instance;
     }
     
